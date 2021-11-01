e9aaa7a13b57f563b5e3fd77cbdff56b587f8efd
YuriiTrofimenko <tyaa@ukr.net>
2021-11-01 21:59:28 +0200

    Todos component comments

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -6,10 +6,15 @@ import { fetchTodos } from '../store/actions'
 
 // eslint-disable-next-line no-shadow
 // @ts-ignore
+// компонент, готовый принимать через props действие вызова получения
+// списка моделей задач и наблюдаемое свойство со списком моделей задач 
 const TodosComponent = ({ fetchTodos, todos }) => {
-    useEffect(() => {
+  // хук, устанавливающий обработчик события монтирования компонента в виртуальный дом
+  useEffect(() => {
+        // вызов действия хранилища: получить откуда-то список моделей задач
         fetchTodos()
-    }, []);
+    }, [fetchTodos]) // наблюдать за внешним (из словаря props) свойством fetchTodos
+    // и вызывать срабатывание обработчика, как только значение этого свойства получено
 
     // @ts-ignore
     return (
@@ -23,11 +28,12 @@ const TodosComponent = ({ fetchTodos, todos }) => {
     </div>
 );
 };
-
+// список свойств состояния для внедрения
 const mapStateToProps = (state: { todos: Todo[] }) => ({
     todos: state.todos,
 })
-
+// список действий
 const mapDispatchToProps = { fetchTodos }
-
+// создание компонента-обертки, не имеющего собственного представления,
+// и внедряющего свойства состояния и действия через props
 export default connect(mapStateToProps, mapDispatchToProps)(TodosComponent)
